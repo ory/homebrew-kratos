5bd5a73179de55a2df17b4c62848461996022192
ory <hi@ory.sh>
2020-01-30 19:09:08 +0100

    Brew formula update for kratos version v0.0.3-alpha.3

--- kratos.rb
+++ kratos.rb
@@ -2,12 +2,12 @@
 class Kratos < Formula
   desc ""
   homepage "https://www.ory.sh"
-  version "0.0.3-alpha.2"
+  version "0.0.3-alpha.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ory/kratos/releases/download/v0.0.3-alpha.2/kratos_0.0.3-alpha.2_macos_amd64.tar.gz"
-    sha256 "3d49661cb464f7971e95e748b5b46850ac9b120bc935fa6ae22065d686e7c9f1"
+    url "https://github.com/ory/kratos/releases/download/v0.0.3-alpha.3/kratos_0.0.3-alpha.3_macos_amd64.tar.gz"
+    sha256 "4af7178296c2e25c9803cb523d02bf2c806293ee555bccf89232f8730be3d6d2"
   elsif OS.linux?
   end
 
