9272cc7348fed14a0fefab1b0d0731b868805890
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-05-05 18:16:14 +0200

    Brew formula update for kratos version v0.6.0-alpha.1.pre.3

--- kratos.rb
+++ kratos.rb
@@ -5,16 +5,16 @@
 class Kratos < Formula
   desc ""
   homepage "https://www.ory.sh"
-  version "0.6.0-alpha.1.pre.2"
+  version "0.6.0-alpha.1.pre.3"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ory/kratos/releases/download/v0.6.0-alpha.1.pre.2/kratos_0.6.0-alpha.1.pre.2-sqlite_macos_64bit.tar.gz"
-    sha256 "5eda395b904a4f35e524ad72b287bf24e984b640ba351d613a60c18a084a45b4"
+    url "https://github.com/ory/kratos/releases/download/v0.6.0-alpha.1.pre.3/kratos_0.6.0-alpha.1.pre.3-sqlite_macos_64bit.tar.gz"
+    sha256 "09e199b5ad080583668dd7929ce5de42735a67bbc4e1b65eb651e5f4e3ca6a40"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ory/kratos/releases/download/v0.6.0-alpha.1.pre.2/kratos_0.6.0-alpha.1.pre.2-sqlite_linux_64bit.tar.gz"
-    sha256 "65afa06d7b8e899a4b0c7377e68126b8fcfe553a3b73ead0beef7e2fb3b74ba5"
+    url "https://github.com/ory/kratos/releases/download/v0.6.0-alpha.1.pre.3/kratos_0.6.0-alpha.1.pre.3-sqlite_linux_64bit.tar.gz"
+    sha256 "87b6ace401eb78f78fa56c7870579bb0e3cd4a211ca8bbd5ec42f8428f3e5654"
   end
 
   def install
